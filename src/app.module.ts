c207afe24a124a63aafb315cf9164c7846d9748a
Sebastian Alfaro <sebas43243@hotmail.com>
2022-07-26 17:23:21 -0500

    added .env for DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductModule, MongooseModule.forRoot('mongodb+srv://admin:balto123@test1.oouow.mongodb.net/product-nest-tutorial')],
+  imports: [ProductModule, MongooseModule.forRoot(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}`)],
   controllers: [AppController],
   providers: [AppService],
 })
