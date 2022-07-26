6382c3c17dfa3468561608ac100f0d542543babf
Sebastian Alfaro <sebas43243@hotmail.com>
2022-07-26 17:29:50 -0500

    added missing config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 import { MongooseModule } from '@nestjs/mongoose';
-
+require('dotenv').config();
 @Module({
-  imports: [ProductModule, MongooseModule.forRoot(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}`)],
+  imports: [
+    ProductModule,
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.MONGODB_DATABASE}`
+    )],
   controllers: [AppController],
   providers: [AppService],
 })
